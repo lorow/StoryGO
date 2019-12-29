96b0b802565f33ad1a4e28370fa0620bef0e6081
lorow <smykupyka@gmail.com>
2019-12-29 21:20:15 +0100

    Add basic subpages

---
+++ frontend/src/pages/generatePage/subpages/index.js
@@ -0,0 +1,7 @@
+import { SuccessEditingSubPage } from './successSubPage';
+import { LinkEditingSubPage } from './linkEdidingSubPage';
+import { FinishingEditingSubPage } from './finishingSubPage';
+
+export { SuccessEditingSubPage }
+export { LinkEditingSubPage }
+export { FinishingEditingSubPage }
---
+++ frontend/src/pages/generatePage/subpages/linkEdidingSubPage.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export function LinkEditingSubPage(props) {
+  return (
+    <div>Gimme gimme linki links</div>
+  )
+}
